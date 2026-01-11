169c86d318beed0e26c669cc4da03193da25b237
DWIKI <dwikiadesaputra360@gmail.com>
2026-01-12 06:34:28 +0700

    Update page.tsx

--- Pemrograman Web/halamanUtama/page.tsx
+++ Pemrograman Web/halamanUtama/page.tsx
@@ -35,7 +35,7 @@ export default function HomePage() {
     },
     {
       title: "Kolam Renang Anak",
-      price: "10.000 / orang",
+      price: "10.000 /orang",
       desc: "Kolam renang khusus anak dengan wahana ember tumpah dan area aman",
       image:
         "https://travelspromo.com/wp-content/uploads/2022/08/Wahana-ember-tumpah-di-kolam-renang-anak.jpg",
@@ -187,6 +187,7 @@ export default function HomePage() {
     </div>
   );
 }
+
 
 
 
